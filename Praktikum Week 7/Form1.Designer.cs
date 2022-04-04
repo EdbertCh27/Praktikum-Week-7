639a4a478689d87406b6ee38530c9652fa6a6b33
EdbertCh27 <edbertch27@gmail.com>
2022-04-04 20:24:46 +0800

    Add project files.

---
+++ Praktikum Week 7/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Praktikum_Week_7
+{
+    partial class FormQuizPanda
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelInputKalimat = new System.Windows.Forms.Label();
+            this.labelInputHuruf = new System.Windows.Forms.Label();
+            this.textBoxInputKal = new System.Windows.Forms.TextBox();
+            this.textBoxInputHur = new System.Windows.Forms.TextBox();
+            this.labelMenjadi = new System.Windows.Forms.Label();
+            this.labelHasil = new System.Windows.Forms.Label();
+            this.textBoxInputMenjadi = new System.Windows.Forms.TextBox();
+            this.buttonKonversi = new System.Windows.Forms.Button();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelInputKalimat
+            // 
+            this.labelInputKalimat.AutoSize = true;
+            this.labelInputKalimat.Location = new System.Drawing.Point(27, 32);
+            this.labelInputKalimat.Name = "labelInputKalimat";
+            this.labelInputKalimat.Size = new System.Drawing.Size(129, 20);
+            this.labelInputKalimat.TabIndex = 0;
+            this.labelInputKalimat.Text = "Masukkan Kalimat";
+            // 
+            // labelInputHuruf
+            // 
+            this.labelInputHuruf.AutoSize = true;
+            this.labelInputHuruf.Location = new System.Drawing.Point(27, 96);
+            this.labelInputHuruf.Name = "labelInputHuruf";
+            this.labelInputHuruf.Size = new System.Drawing.Size(115, 20);
+            this.labelInputHuruf.TabIndex = 1;
+            this.labelInputHuruf.Text = "Masukkan Huruf";
+            // 
+            // textBoxInputKal
+            // 
+            this.textBoxInputKal.Location = new System.Drawing.Point(169, 32);
+            this.textBoxInputKal.Name = "textBoxInputKal";
+            this.textBoxInputKal.Size = new System.Drawing.Size(401, 27);
+            this.textBoxInputKal.TabIndex = 2;
+            // 
+            // textBoxInputHur
+            // 
+            this.textBoxInputHur.Location = new System.Drawing.Point(169, 96);
+            this.textBoxInputHur.Name = "textBoxInputHur";
+            this.textBoxInputHur.Size = new System.Drawing.Size(125, 27);
+            this.textBoxInputHur.TabIndex = 3;
+            // 
+            // labelMenjadi
+            // 
+            this.labelMenjadi.AutoSize = true;
+            this.labelMenjadi.Location = new System.Drawing.Point(363, 99);
+            this.labelMenjadi.Name = "labelMenjadi";
+            this.labelMenjadi.Size = new System.Drawing.Size(63, 20);
+            this.labelMenjadi.TabIndex = 4;
+            this.labelMenjadi.Text = "Menjadi";
+            // 
+            // labelHasil
+            // 
+            this.labelHasil.AutoSize = true;
+            this.labelHasil.Location = new System.Drawing.Point(27, 271);
+            this.labelHasil.Name = "labelHasil";
+            this.labelHasil.Size = new System.Drawing.Size(53, 20);
+            this.labelHasil.TabIndex = 5;
+            this.labelHasil.Text = "Hasil : ";
+            // 
+            // textBoxInputMenjadi
+            // 
+            this.textBoxInputMenjadi.Location = new System.Drawing.Point(445, 96);
+            this.textBoxInputMenjadi.Name = "textBoxInputMenjadi";
+            this.textBoxInputMenjadi.Size = new System.Drawing.Size(125, 27);
+            this.textBoxInputMenjadi.TabIndex = 6;
+            // 
+            // buttonKonversi
+            // 
+            this.buttonKonversi.Location = new System.Drawing.Point(169, 198);
+            this.buttonKonversi.Name = "buttonKonversi";
+            this.buttonKonversi.Size = new System.Drawing.Size(401, 29);
+            this.buttonKonversi.TabIndex = 7;
+            this.buttonKonversi.Text = "Konversi!";
+            this.buttonKonversi.UseVisualStyleBackColor = true;
+            this.buttonKonversi.Click += new System.EventHandler(this.buttonKonversi_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelOutput.Location = new System.Drawing.Point(169, 262);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(34, 31);
+            this.labelOutput.TabIndex = 8;
+            this.labelOutput.Text = "....";
+            // 
+            // FormQuizPanda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.buttonKonversi);
+            this.Controls.Add(this.textBoxInputMenjadi);
+            this.Controls.Add(this.labelHasil);
+            this.Controls.Add(this.labelMenjadi);
+            this.Controls.Add(this.textBoxInputHur);
+            this.Controls.Add(this.textBoxInputKal);
+            this.Controls.Add(this.labelInputHuruf);
+            this.Controls.Add(this.labelInputKalimat);
+            this.Name = "FormQuizPanda";
+            this.Text = "Quiz Panda";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelInputKalimat;
+        private System.Windows.Forms.Label labelInputHuruf;
+        private System.Windows.Forms.TextBox textBoxInputKal;
+        private System.Windows.Forms.TextBox textBoxInputHur;
+        private System.Windows.Forms.Label labelMenjadi;
+        private System.Windows.Forms.Label labelHasil;
+        private System.Windows.Forms.TextBox textBoxInputMenjadi;
+        private System.Windows.Forms.Button buttonKonversi;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
+
---
+++ Praktikum Week 7/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praktikum_Week_7
+{
+    public partial class FormQuizPanda : Form
+    {
+        //char[] huruf;
+        public FormQuizPanda()
+        {
+            InitializeComponent();
+        }
+
+
+        //char cekHuruf;
+        public void buttonKonversi_Click(object sender, EventArgs e)
+        {
+            //char[] koleksiHuruf = { 'A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' }; 
+            
+            
+            string input = textBoxInputKal.Text.ToUpper();
+            labelOutput.Text = "";
+            foreach (var huruf in input)
+            {
+                
+                string inputMenjadi = textBoxInputMenjadi.Text;
+                string inputHur = textBoxInputHur.Text;
+
+                foreach (var hurufInput in inputHur)
+                {
+                    int IndexHurufInput = Convert.ToInt32(hurufInput); //CARI VALUE
+                    foreach (var hurufMenjadi in inputMenjadi)
+                    {
+                        int indexHurufMenjadi = Convert.ToInt32(hurufMenjadi);
+
+                        int selisih = indexHurufMenjadi - IndexHurufInput;
+                        string output = Convert.ToChar(Convert.ToInt32(huruf) + selisih).ToString();
+
+                        if(Convert.ToInt32(huruf) <= 65)
+                        {
+                            int setValue = 90;
+                            int valueHuruf = Convert.ToInt32(huruf);
+                            valueHuruf = setValue;
+                            //labelOutput.Text += output.ToUpper();
+                            
+                        }
+                        else if (Convert.ToInt32(huruf) >= 90)
+                        {
+                            int setValue = 65;
+                            int valueHuruf = Convert.ToInt32(huruf);
+                            valueHuruf = setValue;
+                            //labelOutput.Text += output.ToUpper();
+                        }
+                        labelOutput.Text += output.ToUpper();
+
+
+
+                        //MessageBox.Show(IndexHurufInput.ToString()); //97
+                        //MessageBox.Show(indexHurufMenjadi.ToString()); //122
+                    }
+
+                }
+                /*for (int i = 0; i <input.Length; i++)
+                {
+                    foreach (var hurufInput in inputHur)
+                    {
+                        int IndexHurufInput = Convert.ToInt16(hurufInput); //CARI VALUE
+                        foreach (var hurufMenjadi in inputMenjadi)
+                        {
+                            int indexHurufMenjadi = Convert.ToInt16(hurufMenjadi);
+
+                            int selisih = indexHurufMenjadi - IndexHurufInput;
+                            string output = Convert.ToChar(Convert.ToInt32(huruf) + selisih).ToString();
+                            labelOutput.Text = output;
+                        }
+
+                    }
+                    
+                }
+                */
+                /*foreach (var hurufInput in inputHur)
+                {
+                    int IndexHurufInput = Convert.ToInt16(hurufInput); //CARI VALUE
+                    foreach (var hurufMenjadi in inputMenjadi)
+                    {
+                        int indexHurufMenjadi = Convert.ToInt16(hurufMenjadi);
+                        
+                        int selisih = indexHurufMenjadi - IndexHurufInput;
+                        string output = Convert.ToChar(Convert.ToInt32(huruf) + selisih).ToString();
+                        labelOutput.Text = output;
+                    }
+                    
+                } */
+                
+                
+                
+                
+            }
+            
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            char huruf1 = 'a';
+            char huruf2 = 'd';
+            int selisih = huruf2 - huruf1;
+            MessageBox.Show(selisih.ToString());
+            */
+        }
+
+        /*
+        string input = textBoxInputKal.Text;
+            
+            foreach (var huruf in input)
+            {
+                
+                string inputMenjadi = textBoxInputMenjadi.Text;
+        string inputHur = textBoxInputHur.Text;
+                //for(int i = 0; i <input.Length; i++)
+                foreach (var hurufInput in inputHur)
+                {
+                    int IndexHurufInput = Convert.ToInt16(hurufInput); //CARI VALUE
+                    foreach (var hurufMenjadi in inputMenjadi)
+                    {
+                        int indexHurufMenjadi = Convert.ToInt16(hurufMenjadi);
+
+        int selisih = indexHurufMenjadi - IndexHurufInput;
+        string output = Convert.ToChar(Convert.ToInt32(huruf) + selisih).ToString();
+        labelOutput.Text = output;
+                    }
+
+}
+                
+                
+                
+                
+            }
+
+
+        //if (cekHuruf.ToString().Contains(textBoxInputKal.Text))
+       
+
+    //textBoxInputKal.Text;
+
+    /*
+    if (textBoxInputHur.Text.Contains(textBoxInputKal.Text)) ;
+    {
+        if (textBoxInputHur.Text = 'a')
+            textBoxInputKal.Text = textBoxInputKal.Text.StartsWith('a');
+    }
+    */
+
+    //huruf = labelInputKalimat.Text.ToCharArray();
+
+
+    //koleksiHuruf = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+
+    //labelOutput.Text = input;
+
+
+    //string inputMenjadi = textBoxInputMenjadi.Text;
+
+    //string inputHur = Convert.ToInt16(textBoxInputHur.Text).ToString();
+    //string inputMenjadi = Convert.ToInt16(textBoxInputMenjadi).ToString();
+    //MessageBox.Show(inputHur.ToString());
+    /*
+    string inputHur = textBoxInputHur.Text;
+    foreach (var huruf in inputHur)
+    {
+        Convert.ToInt16(huruf).ToString(); //CARI VALUE
+    //MessageBox.Show(Convert.ToChar(Convert.ToInt16(huruf) + selisih).ToString());
+    }
+    string inputMenjadi = textBoxInputMenjadi.Text;
+    foreach (var huruf in inputMenjadi)
+    {
+        Convert.ToInt16(huruf).ToString();
+    }
+    */
+
+
+
+    //string inputHur = Convert.ToInt16(textBoxInputHur.Text).ToString();
+    //string inputMenjadi = Convert.ToInt16(textBoxInputMenjadi).ToString();
+    //MessageBox.Show(inputHur.ToString());
+    //MessageBox.Show(inputHur.ToString());
+    //MessageBox.Show(Convert.ToInt16(huruf).ToString()); //CARI VALUE
+
+    //int selisih = inputMenjadi - inputHur;
+    //int selisih = (Convert.ToInt32(inputMenjadi)) - (Convert.ToInt32(inputHur));
+    //Convert.ToChar(Convert.ToInt32(kata) + selisih).ToString();
+        //MessageBox.Show(Convert.ToChar(Convert.ToInt16(huruf) + selisih).ToString());
+    }
+}
